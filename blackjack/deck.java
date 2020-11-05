45c64b36202961410046a21a75279f82e91145f8
Fumbledumble <niravkoley@gmail.com>
2020-11-05 09:52:30 -0600

    Fixed Shuffle

--- blackjack/deck.java
+++ blackjack/deck.java
@@ -1,21 +1,31 @@
-import java.util.*;
+import java.util.Random;
 
 public class deck{
-  card[] deck;
-  int length;
+card[] deckOfCards;
 
   public deck(){
-    deck = new card[52]; // Creates array storing object
+    deckOfCards = new card[52]; // Creates an array of objects
 
     for (int i = 1; i <= 4; i++){ // loop for each suit
       for (int z = 1; z <= 13; z++){ // loop for each strValue
-        deck[((z+(13*(i-1))-1))] = new card(i, z);
+        deckOfCards[((z+(13*(i-1))-1))] = new card(i, z);
       }
     }
   }
 
+  public void shuffle(){
+    for(int i = (deckOfCards.length-1); i >= 0; i--){
+      Random randy = new Random();
+
+      int rand = randy.nextInt(52);
+      card temp = deckOfCards[i];
+      deckOfCards[rand] = deckOfCards[i];
+      deckOfCards[i] = temp;
+    }
+  }
+
   public card getCard(int i){
-      card theCard = deck[i];
+      card theCard = deckOfCards[i];
     return theCard;
   }
 }
--- blackjack/game.java
+++ blackjack/game.java
@@ -4,8 +4,14 @@ public class game {
   public static void main(String[] args){
     deck deckOfCards = new deck();
 
-    for (int i = 1; i < 52; i++){
+    for (int i = 0; i < 52; i++){
     deckOfCards.getCard(i).display();
+    }
+
+    deckOfCards.shuffle();
+
+    for (int i = 0; i < 52; i++){
+      deckOfCards.getCard(i).display();
     }
   }
 }
