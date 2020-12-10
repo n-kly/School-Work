2d7d9629ca500099fb4b10635a3b018e0b85ff68
Fumbledumble <niravkoley@gmail.com>
2020-12-09 23:58:30 -0600

    Did the entire thing

--- dataSet/SWGUI.java
+++ dataSet/SWGUI.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.util.*;
 import java.util.Scanner;
 import java.io.File;
+import java.util.StringTokenizer;
 
 public class SWGUI {
     private JButton nextButton;
@@ -15,6 +20,10 @@ public class SWGUI {
     private JTextField textField8;
     private JTextField textField9;
     private JPanel salam;
+    private JTextField textField10;
+    private JTextField searchTextField;
+    public int counter = 0;
+    public ArrayList<setMaker> swChar;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SWGUI");
@@ -24,11 +33,61 @@ public class SWGUI {
         frame.setVisible(true);
     }
 
-    public SWCharOutput(){
-        File swFile = new File(); // TODO
-        Scanner FileInp = new Scanner(swFile); // TODO
+    public SWGUI(){
+        swChar = new ArrayList();
+        try {
+            File swFile = new File("C:\\Users\\nirav\\Desktop\\Code\\School-Work\\dataSet\\characters.csv");
+            Scanner fileInp = new Scanner(swFile);
+            fileInp.nextLine();
+            while(fileInp.hasNext()) {
+                StringTokenizer breaker = new StringTokenizer(fileInp.nextLine(), ",");
+
+                setMaker temp = new setMaker(breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken(),breaker.nextToken());
+                temp.displayChar();
+                swChar.add(temp);
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File doesn't exist on this computer");
+        }
+        display();
+
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (counter==(swChar.size()-1)){
+                    counter = counter;
+                } else {
+                    counter = counter + 1;
+                }
+                display();
+            }
+        });
+        previousButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (counter==0){
+                    counter = counter;
+                } else {
+                    counter = counter - 1;
+                }
+                display();
+            }
+        });
     }
 
+    public void display(){
+        textField1.setText(swChar.get(counter).name);
+        textField2.setText(swChar.get(counter).height);
+        textField3.setText(swChar.get(counter).mass);
+        textField4.setText(swChar.get(counter).hairColor);
+        textField5.setText(swChar.get(counter).skinColor);
+        textField10.setText(swChar.get(counter).eyeColor);
+        textField6.setText(swChar.get(counter).birthYear);
+        textField7.setText(swChar.get(counter).gender);
+        textField8.setText(swChar.get(counter).homeworld);
+        textField9.setText(swChar.get(counter).species);
+    }
 }
 
 
--- dataSet/setMaker.java
+++ dataSet/setMaker.java
@@ -1,3 +1,30 @@
 public class setMaker {
-    
+    String name;
+    String height;
+    String mass;
+    String hairColor;
+    String skinColor;
+    String eyeColor;
+    String birthYear;
+    String gender;
+    String homeworld;
+    String species;
+
+    public setMaker(String name, String height, String mass, String hairColor, String skinColor, String eyeColor, String birthYear, String gender, String homeworld, String species){
+        this.name = name;
+        this.height = height;
+        this.mass = mass;
+        this.hairColor = hairColor;
+        this.skinColor = skinColor;
+        this.eyeColor = eyeColor;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.homeworld = homeworld;
+        this.species = species;
+    }
+
+    public void displayChar(){
+        System.out.println(name+","+height+","+mass+","+hairColor+","+skinColor+","+eyeColor+","+birthYear+","+gender+","+homeworld+","+species);
+    }
 }
+
